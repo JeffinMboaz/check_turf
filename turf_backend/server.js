2c1810eb58d7f6633b382d0d262ebeab73fa37d9
JeffinMBoaz <jeffinmboaz@gmail.com>
2025-05-16 12:48:38 +0530

    frontend url added

--- turf_backend/server.js
+++ turf_backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173","https://check-turf-f6fw.vercel.app"],
         methods:["GET","POST","DELETE","PUT","PATCH ","OPTIONS"],
         credentials: true,
       }
